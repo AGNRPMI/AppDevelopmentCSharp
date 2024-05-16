efb8f36970cd052e8d154970f468f1b8f875d47a
AGNRPMI <pmi.petrov@yandex.ru>
2024-05-16 20:29:16 +0400

    add Seminar03

---
+++ Seminar03/Labirint.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seminar03
+{
+    public class Labirint
+    {
+        public int[,]? array;
+        public int[,] ChooseLabirint()
+        {
+            Console.WriteLine("выбери лабиринт 1, 2 или 3");
+            int type = Convert.ToInt32(Console.ReadLine());
+
+            switch (type)
+            {
+                default:
+                case 1:
+                    array = new int[,]
+                    {
+                        {1, 1, 1, 1, 1, 1, 1 },
+                        {1, 0, 0, 0, 0, 0, 1 },
+                        {1, 0, 1, 1, 1, 0, 1 },
+                        {2, 0, 0, 0, 1, 0, 2 },
+                        {1, 1, 0, 0, 1, 1, 1 },
+                        {1, 1, 1, 0, 1, 1, 1 },
+                        {1, 1, 1, 2, 1, 1, 1 }
+                    };
+                    return array;
+                case 2:
+                    array = new int[,]
+                    {
+                        {1, 1, 1, 2, 1, 1, 1 },
+                        {1, 0, 0, 0, 0, 0, 1 },
+                        {1, 0, 1, 1, 1, 0, 1 },
+                        {2, 0, 0, 0, 1, 0, 2 },
+                        {1, 1, 0, 0, 1, 1, 1 },
+                        {1, 1, 1, 0, 1, 1, 1 },
+                        {1, 1, 1, 2, 1, 1, 1 }
+                    };
+                    return array;
+                case 3:
+                    array = new int[,]
+                    {
+                        {1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1 },
+                        {1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1 },
+                        {1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1 },
+                        {2, 0, 0, 0, 1, 0, 1, 0, 0, 0, 2 },
+                        {1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1 },
+                        {1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1 },
+                        {1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 2 },
+                        {1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1 },
+                        {1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1 },
+                        {1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1 },
+                        {1, 1, 1, 2, 1, 1, 1, 2, 1, 1, 1 }
+                    };
+                    return array;
+            }
+        }
+
+        public void PrintArray()
+        {
+            int width = array.GetLength(0);
+            int heigth = array.GetLength(1);
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < heigth; j++)
+                    Console.Write(array[i, j] + " ");
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+        public bool HasExit()
+        {
+            Console.WriteLine("Стартовая точка в центре массива");
+            int startI = array.GetLength(0)/2;
+            int startJ = array.GetLength(1)/2 ;
+            Stack<Tuple<int, int>> stack = new();
+            stack.Push(new(startI, startJ));
+            int i = 0;
+            
+            while (stack.Count > 0)
+            {
+                var temp = stack.Pop();
+                if (array[temp.Item1, temp.Item2] == 2)
+                {
+                    Console.WriteLine($"Выход найден! в точке {temp.Item1+1},{temp.Item2+1}");
+                    //return true;
+                    i++;
+                }
+                array[temp.Item1, temp.Item2] = 1;
+                try
+                {
+                    if (temp.Item2 >= 0 && array[temp.Item1, temp.Item2 - 1] != 1)
+                        stack.Push(new(temp.Item1, temp.Item2 - 1)); // вверх
+
+                    if (temp.Item2 + 1 < array.GetLength(1) && array[temp.Item1, temp.Item2 + 1] != 1)
+                        stack.Push(new(temp.Item1, temp.Item2 + 1)); // низ
+
+                    if (temp.Item1 >= 0 && array[temp.Item1 - 1, temp.Item2] != 1)
+                        stack.Push(new(temp.Item1 - 1, temp.Item2)); // влево
+
+                    if (temp.Item1 + 1 < array.GetLength(0) && array[temp.Item1 + 1, temp.Item2] != 1)
+                        stack.Push(new(temp.Item1 + 1, temp.Item2)); // вправо
+                }
+                catch
+                {
+                    startI = array.GetLength(0) / 2;
+                    startJ = array.GetLength(1) / 2;
+                }
+            }
+            Console.WriteLine($"Количество выходов: {i}"); 
+            return false;
+        }
+
+    }
+}
---
+++ Seminar03/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Text;
+
+namespace Seminar03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isOpen = true;
+            while (isOpen)
+            {
+                isOpen = WorkingProcess(isOpen);
+                if (isOpen)
+                {
+                    PrintTaskDescription();
+                    Labirint labirint = new Labirint();
+                    labirint.ChooseLabirint();
+                    labirint.PrintArray();
+                    labirint.HasExit();
+
+                }
+            }
+            Console.Write("Нажмите любую клавишу для завершения ...");
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        static void PrintTaskDescription()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine();
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("Доработайте приложение поиска пути в лабиринте, но на этот раз вам нужно определить сколько всего выходов имеется в лабиринте");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+
+        static bool WorkingProcess(bool _isOpen)
+        {
+            string _choice = "none";
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine("Желаете выполнить алгоритм? (y/n)");
+
+            while (_choice != "y" || _choice != "n")
+            {
+                Console.WriteLine("введите команду: ");
+                _choice = Console.ReadLine();
+                switch (_choice)
+                {
+                    case "y":
+                        Console.WriteLine("Выполняется функция ...");
+                        Console.WriteLine(" ");
+                        _isOpen = true;
+                        break;
+                    case "n":
+                        Console.WriteLine("Завершение программы... Нажмите Enter");
+                        Console.WriteLine(" ");
+                        _isOpen = false;
+                        break;
+                    default:
+                        Console.WriteLine("команда не распознана, введите еще раз");
+                        Console.WriteLine(" ");
+                        break;
+                }
+                if (_choice == "y" || _choice == "n") break;
+            }
+
+            Console.ForegroundColor = ConsoleColor.White;
+            return _isOpen;
+        }
+    }
+}
+
