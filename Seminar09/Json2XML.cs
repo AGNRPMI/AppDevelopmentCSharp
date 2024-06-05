72e23feb16d13eb06c6ac31ab1a7c85758edbfb5
AGNRPMI <pmi.petrov@yandex.ru>
2024-06-05 12:10:56 +0400

    add Seminar09

---
+++ Seminar09/Json2XML.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Xml;
+
+namespace Seminar09
+{
+    public class Json2XML
+    {
+        private string str;
+
+        public Json2XML(string _str)
+        {
+            this.str = _str;
+        }
+        public void Use()
+        {
+            var json = JsonDocument.Parse(str);
+
+            var xml = new XmlDocument();
+            var xmlRoot = xml.CreateElement("root");
+            xml.AppendChild(xmlRoot);
+            ConvertJson2Xml(json.RootElement, xmlRoot);
+            Console.WriteLine(xml.OuterXml);
+        }
+        static void ConvertJson2Xml(JsonElement jsonElement, XmlElement xmlParent)
+        {
+            foreach (var prop in jsonElement.EnumerateObject())
+            {
+                if (prop.Value.ValueKind == JsonValueKind.Object)
+                {
+                    var xmlElement = xmlParent.OwnerDocument.CreateElement(prop.Name);
+                    xmlParent.AppendChild(xmlElement);
+                    ConvertJson2Xml(prop.Value, xmlElement);
+                }
+                else
+                {
+                    var xmlElement = xmlParent.OwnerDocument.CreateElement(prop.Name);
+                    xmlElement.InnerText = prop.Value.ToString();
+                    xmlParent.AppendChild(xmlElement);
+                }
+            }
+        }
+    }
+}
---
+++ Seminar09/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Text;
+using System.Text.Json;
+using System.Xml;
+
+namespace Seminar09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isOpen = true;
+            while (isOpen)
+            {
+                isOpen = WorkingProcess(isOpen);
+                if (isOpen)
+                {
+                    PrintTaskDescription();
+                    // блок кода для исполнения программы
+                    string jsonStr = "{\"name\":\"фывфыв\",\"age\":999,\"city\":\"выаыавц\"}";
+                    Json2XML j2x = new Json2XML(jsonStr);
+
+                    j2x.Use();
+
+                }
+            }
+            Console.Write("Нажмите любую клавишу для завершения ...");
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        static void PrintTaskDescription()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine();
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("Напишите приложение, конвертирующее произвольный JSON в XML. Используйте JsonDocument.");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+
+        static bool WorkingProcess(bool _isOpen)
+        {
+            string _choice = "none";
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine("Желаете выполнить алгоритм? (y/n)");
+
+            while (_choice != "y" || _choice != "n")
+            {
+                Console.WriteLine("введите команду: ");
+                _choice = Console.ReadLine();
+                switch (_choice)
+                {
+                    case "y":
+                        Console.WriteLine("Выполняется функция ...");
+                        Console.WriteLine(" ");
+                        _isOpen = true;
+                        break;
+                    case "n":
+                        Console.WriteLine("Завершение программы... Нажмите Enter");
+                        Console.WriteLine(" ");
+                        _isOpen = false;
+                        break;
+                    default:
+                        Console.WriteLine("команда не распознана, введите еще раз");
+                        Console.WriteLine(" ");
+                        break;
+                }
+                if (_choice == "y" || _choice == "n") break;
+            }
+
+            Console.ForegroundColor = ConsoleColor.White;
+            return _isOpen;
+        }
+    }
+}
+
